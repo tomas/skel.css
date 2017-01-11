4e1a02f26243e8bea3fa8df22d56710bffe36708
Tomás Pollak <tomaspollak@gmail.com>
2017-01-11 10:46:09 -0800

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+mv css build
+git checkout gh-pages
+[ $? -ne 0 ] && echo "Stopping here." && exit 1
+
+rm -Rf dist/css
+mv build dist/css
+git commit -a -m "Updated build."
+git push -f origin gh-pages
+git checkout master
