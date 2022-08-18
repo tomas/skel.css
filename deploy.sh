01460046d9aff4c850e76035276ae66372c8c1e8
Tomás Pollak <tomaspollak@gmail.com>
2022-08-18 00:39:45 -0400

    Add plugins.css (nice select only, for now)

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,9 @@ rm -f dist/latest
 mkdir -p $path
 
 cat css/normalize.css css/skeleton.css css/extras.css > ${path}/skel.css
+cat css/normalize.css css/skeleton.css css/extras.css css/plugins.css > ${path}/skel-with-plugins.css
 csso -i ${path}/skel.css -o ${path}/skel.min.css
+csso -i ${path}/skel-with-plugins.css -o ${path}/skel-with-plugins.min.css
 
 cd dist
 ln -sf "$version" latest
