934e7495b69da6e3e601708453a140632d6df022
Tomás Pollak <tomaspollak@gmail.com>
2024-03-14 00:09:16 -0300

    Add skel-base.css build

--- deploy.sh
+++ deploy.sh
@@ -9,20 +9,43 @@ rm -Rf $path
 rm -f dist/latest
 mkdir -p $path
 
-cat css/normalize.css css/skeleton.css css/components.css > ${path}/skel.css
-cat ${path}/skel.css css/extras/*.css > ${path}/skel-with-extras.css
+base_extras="
+css/extras/details.css
+css/extras/alerts.css
+css/extras/close-button.css
+css/extras/menus.css
+css/extras/dropdown.css
+css/extras/icons.css
+css/extras/bg-cover.css
+"
+
+other_extras="
+css/extras/animations.css
+css/extras/bselect.css
+css/extras/card-list.css
+css/extras/checkboxes.css
+css/extras/modal.css
+css/extras/nice-select.css
+css/extras/spinners.css
+css/extras/toasts.css
+css/extras/tooltips.css
+"
+
+cat css/normalize.css css/skeleton.css > ${path}/skel-bare.css
+cat css/normalize.css css/skeleton.css $base_extras > ${path}/skel.css
+cat ${path}/skel.css $other_extras > ${path}/skel-with-extras.css
 csso -i ${path}/skel.css -o ${path}/skel.min.css
 csso -i ${path}/skel-with-extras.css -o ${path}/skel-with-extras.min.css
 
-cd dist
-ln -sf "$version" latest
-git add dist
-git commit -a -m "Updated build."
+# cd dist
+# ln -sf "$version" latest
+# git add dist
+# git commit -a -m "Updated build."
 
-git checkout gh-pages
-[ $? -ne 0 ] && echo "Stopping here." && exit 1
+# git checkout gh-pages
+# [ $? -ne 0 ] && echo "Stopping here." && exit 1
 
-# git merge --ff-only master
-git merge master
-git push origin
-git checkout master
+# # git merge --ff-only master
+# git merge master
+# git push origin
+# git checkout master
